52df585cbbd6e705530800b1196ff0643094309f
Belo-Osagie Eseosa <eseosadaboy@gmail.com>
2025-05-20 20:27:49 -0400

    testing kafka connection

--- src/ingest.py
+++ src/ingest.py
@@ -1,4 +1,3 @@
-# src/ingestion/kafka_producer.py
 import time
 import pandas as pd
 from kafka import KafkaProducer
@@ -12,16 +11,13 @@ producer = KafkaProducer(
 # Load and preprocess your dataset (e.g., NAB)
 df = pd.read_csv('data//PerformanceGraphExport.csv')
 df = df[['Effective date ', 'S&P 500']]
-
-print(df.head())
-
 df=df.dropna()
+
 print(df)
 
 
-'''
+
 for _, row in df.iterrows():
     event = row.to_dict()
     producer.send('anomaly-topic', value=event)
-    time.sleep(1)  # Simulate streaming every 1s
-''' 
+    #time.sleep(1)  # Simulate streaming every 1s
