38b5fe397bc59677a834683354a26d3abc813860
Belo-Osagie Eseosa <eseosadaboy@gmail.com>
2025-05-20 20:44:33 -0400

    finally got kafka producer working

--- src/ingest.py
+++ src/ingest.py
@@ -4,7 +4,7 @@ from kafka import KafkaProducer
 import json
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='localhost:9094',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
